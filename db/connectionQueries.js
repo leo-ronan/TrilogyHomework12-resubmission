052438c41d45e75af3a4b12c20b76f0171b2c7a5
leo-ronan <leoronan15@gmail.com>
2021-01-29 00:36:35 -0500

    Added all function templates that will be needed

---
+++ db/connectionQueries.js
@@ -0,0 +1,42 @@
+function displayAllEmployees() {
+
+}
+function displayEmployeesByDepartment() {
+
+}
+
+function displayEmployeesByRole() {
+    
+}
+
+function addEmployee() {
+    
+}
+
+function addDepartment() {
+    
+}
+
+function addRole() {
+    
+}
+
+function updateEmployee() {
+    
+}
+
+function updateDepartment() {
+    
+}
+
+function updateRole() {
+    
+}
+
+function fireAll() {
+
+}
+
+function exit() {
+    
+}
